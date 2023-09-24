b80fb8cb4a3c915d1dfcc1850f51a8077988a2c9
Insiro <leeon@insiro.me>
2023-09-24 16:29:20 +0900

    add: action type

--- src/type.py
+++ src/type.py
@@ -9,9 +9,11 @@ class TextInstance(TypedDict):
     type: str
     hash: str
 
+
 class NpcSelect(TypedDict):
-    language:str
-    npcName:str
+    language: str
+    npcName: str
+
 
 ScriptSection = dict[str, TextInstance]
 """key: hash"""
@@ -19,3 +21,25 @@ LangSplitedScript = dict[str, ScriptSection]
 """key: Language"""
 FileKeyScriptSection = dict[str, TextInstance]
 """key: filepath without Language"""
+
+
+class Action:
+    src_hash: str
+    src_ext: str
+    dst_hash: str
+    dst_ext: str
+
+    def __init__(
+        self,
+        src_hash: str,
+        dst_hash: str,
+        src_ext: str,
+        dst_ext: str,
+    ) -> None:
+        self.src_ext = src_ext
+        self.src_hash = src_hash
+        self.dst_ext = dst_ext
+        self.dst_hash = dst_hash
+
+
+HashedActions = dict[str, Action]
