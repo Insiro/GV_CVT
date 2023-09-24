d1794fe685da39d3bb486600ee399e12f31313a0
Insiro <leeon@insiro.me>
2023-09-24 15:50:10 +0900

    refactor: split apply section

--- src/cvt_file.py
+++ src/cvt_file.py
@@ -1,5 +1,7 @@
 from .type import *
-from os import path, listdir, rename
+from os import path, listdir, rename, makedirs
+from shutil import rmtree, copyfile
+from .config import Config
 
 
 def rmLangPath(fileName: str) -> str:
@@ -14,26 +16,61 @@ def cvtScriptFile2Key(script: ScriptSection) -> FileKeyScriptSection:
     return output
 
 
-def convertName(
-    from_scritp: ScriptSection, to_script: FileKeyScriptSection, input_dir: str
+class Action:
+    src_hash: str
+    src_ext: str
+    dst_hash: str
+    dst_ext: str
+
+    def __init__(
+        self,
+        src_hash: str,
+        dst_hash: str,
+        src_ext: str,
+        dst_ext: str,
+    ) -> None:
+        self.src_ext = src_ext
+        self.src_hash = src_hash
+        self.dst_ext = dst_ext
+        self.dst_hash = dst_hash
+
+
+def convert_hash(
+    from_scritp: ScriptSection, to_script: FileKeyScriptSection, mod_dir: str
 ):
-    assert path.isdir(input_dir)
-    for dname in listdir(input_dir):
-        dir = path.join(input_dir, dname)
+    actions: dict[str, Action] = {}
+    assert path.isdir(mod_dir)
+    for ext in listdir(mod_dir):
+        dir = path.join(mod_dir, ext)
         if not path.isdir(dir):
             continue
         for item in listdir(dir):
             fsplit = item.split(".")
             if len(fsplit) != 2:
                 continue
+
             from_hash = fsplit[0]
             if from_hash not in from_scritp:
                 continue
 
-            key = rmLangPath(from_scritp[from_hash]["fileName"])
-            new_hash = to_script[key]["hash"]
-            original = path.join(dir, from_hash + ".wem")
-            target = path.join(dir, new_hash + ".wem")
+            f_key = rmLangPath(from_scritp[from_hash]["fileName"])
+            new_hash = to_script[f_key]["hash"]
 
-            rename(original, target)
-            print(original, target)
+            actions[from_hash] = Action(
+                src_ext=ext, dst_ext=ext, src_hash=from_hash, dst_hash=new_hash
+            )
+    return actions
+
+
+def apply(config: Config, actions: dict[str, Action]):
+    config.input
+    config.wem
+
+    rmtree(config.wem)
+    for hash, item in actions.items():
+        makedirs(path.dirname(path.join(config.wem, item.dst_ext)), exist_ok=True)
+        from_path = path.join(config.input, config._from["language"])
+        copyfile(
+            path.join(from_path, item.src_ext, item.src_hash + ".wem"),
+            path.join(config.wem, item.dst_ext, item.dst_hash + ".wem"),
+        )
